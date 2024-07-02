a06b6cab5a2f781e9cefed20a7f1c897ae550863
Raden-nvii <navinugraha15@gmail.com>
2024-07-02 09:51:31 +0700

    feat: practice logic

--- month-2/day-10/day-10.js
+++ month-2/day-10/day-10.js
@@ -11,7 +11,7 @@ let contacts = [
     phoneNumber: "62888092131232",
     address: "Jl. Bikini Bottom",
     jobTitle: "CEO",
-    label: [label[0]],
+    labels: [labels[0]],
   },
   {
     id: 2,
@@ -20,7 +20,7 @@ let contacts = [
     phoneNumber: "62888099912323",
     address: "Jl. Pasific Ocean",
     jobTitle: "Cleaner",
-    label: [label[0]],
+    labels: [labels[0]],
   },
   {
     id: 3,
@@ -29,8 +29,92 @@ let contacts = [
     phoneNumber: "6288809990023",
     address: "Jl. Merdeka Raya",
     jobTitle: "Software Engineer",
-    label: [label[0]],
+    label: [labels[0]],
   },
 ];
+console.log([labels, contacts]);
 
-console.log([label, contact]);
+// Variables and Data Types
+const name = "navi";
+const age = 23;
+const output = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(output);
+console.log(`Hello, my name is ${name} and I am ${age} years old.`);
+
+// Basic Arithmetic Operations
+let a = 10;
+let b = a + 10;
+
+let add = a + b;
+console.log(add);
+
+let minus = b - a - 1;
+console.log(minus);
+let divide = b / 2;
+console.log(divide);
+
+// Arrays;
+
+const myArray = ["apple", "banana", "orange"];
+
+const firstElement = myArray[0];
+const lastElement = myArray[myArray.length - 1];
+console.log(firstElement);
+console.log(lastElement);
+
+// Objects
+const person = { firstName: "Muhammad", lastName: "Nugraha", age: 24 };
+console.log(
+  `Hello, my name is ${person.firstName} ${person.lastName} and I am ${person.age} years old.`
+);
+
+// Conditional Statements
+function checkOddorEven(number) {
+  return number % 2 === 0 ? "the number is even" : "the number is odd";
+}
+
+// let userInput = prompt("Enter a number: ");
+// let number = parseInt(userInput, 10);
+console.log(checkOddorEven(2));
+
+// Loops
+let text = "";
+for (let i = 0; i < 10; i++) {
+  text += "the number is " + i + "\n";
+}
+
+console.log(text);
+
+// String Manipulation
+function reverseString(x) {
+  return x.split("").reverse().join("");
+}
+
+console.log(reverseString("String"));
+
+// Array Manipulation
+const myArray2 = [10, 5, 3, 24, 50];
+
+function biggestNumber() {
+  return Math.max(...myArray2);
+}
+
+console.log(biggestNumber());
+
+// Object Methods
+let car = { make: "Honda", model: "Sedan", year: "2020" };
+
+function carDetails() {
+  return `This car is a ${car.year} ${car.make} ${car.model}.`;
+}
+
+console.log(carDetails());
+
+// Functions
+const numbers = [1, 3, 4, 5, 7, 78];
+
+function evenNumbers(arr) {
+  return arr.filter((number) => number % 2 === 0);
+}
+
+console.log(evenNumbers(numbers));
--- month-2/day-10/day-10.md
+++ month-2/day-10/day-10.md
@@ -1,16 +1,20 @@
 # Day 10: JavaScript, Console, Variables, Types, Debugging
 
-  ## Task
-  - Create a variable to store the Address Book data, such as contacts, since Address Book is the application name.
-    - The contacts data would be organized as array of objects
-    - The contact have several fields such as ID, Full Name, Phone, Email, Location, etc.
-    - Console log the data.
+## Task
 
-    You could check the work on [this file](https://github.com/navi-0115/bearmentor-logbook-navi/blob/main/month-2/day-10/day-10.js)
-    
-    ## Results
-    <img src="/month-2/day-10/image.png" alt="Screenshot of the project" width="600"/>
+- Create a variable to store the Address Book data, such as contacts, since Address Book is the application name.
 
-    ## Learn
-    I practice basic javascript through **[freecodecamp](https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/) ** to enhance understandings of javascript programming
-    ![freecodecamp](image-1.png)
+  - The contacts data would be organized as array of objects
+  - The contact have several fields such as ID, Full Name, Phone, Email, Location, etc.
+  - Console log the data.
+
+  You could check the work on [this file](https://github.com/navi-0115/bearmentor-logbook-navi/blob/main/month-2/day-10/day-10.js)
+
+  ## Results
+
+  <img src="/month-2/day-10/assets/image.png" alt="Screenshot of the project" width="600"/>
+
+  ## Learn
+
+  I practice basic javascript through **[freecodecamp](https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/) ** to enhance understandings of javascript programming
+  ![freecodecamp](/month-2/day-10/assets/image-1.png)
